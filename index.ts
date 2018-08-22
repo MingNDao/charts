0c6c3f7bc0545de527c55b781063d95d5534a0cb
杜方宇 <dfy30866@qq.com>
2018-08-22 08:27:09 +0800

    更新数据

--- index.ts
+++ index.ts
@@ -12,33 +12,37 @@ let g = new G()
 
 let rc_1 = new Rec({
   left: 100,
-  top: 100,
+  top: m.h / 10,
   w: m.w - 150,
-  h: 400,  
+  h: m.h / 2,  
   data: []
 })
 
 let start = 2800
-let data_1 = Array.apply(null, {length: 241}).map(item => {
-  return start += Math.random() * (start * .003) - start * .0015
-})
-
-let _data = Array.apply(null, {length: 241}).map(item => {
-  return Math.random() * 500 + 500
-})
+let data_1 = []
+let data_2 = []
 
 rc_1.update(data_1)
 
 let t_1 = new temp({
   left: 100,
-  top: 500,  
+  top: m.h/2 + m.h / 10,  
   w: m.w - 150,
-  h: 200,
-  data: _data
+  h: m.h * 3 / 10,
+  data: []
 })
 
 g.add(rc_1)
 g.add(t_1)
 
 m.add(g)
-m.render()
+const t = setInterval(function() {
+  if (data_1.length >= 241 ) {
+    return clearInterval(t)
+  }
+  data_1.push(start += Math.random() * (start * .003) - start * .0015)
+  data_2.push(Math.random() * 500 + 500)
+  rc_1.update(data_1)
+  t_1.update(data_2)
+  m.render()
+}, 1000 / 12)
