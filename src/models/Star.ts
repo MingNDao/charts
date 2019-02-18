45a0c2d68f57cf37153a41fc8149a704a0490f62
yzajoee <yzajoee@users.noreply.github.com>
2019-02-18 09:55:05 +0800

    star移动更改

--- src/models/Star.ts
+++ src/models/Star.ts
@@ -6,6 +6,8 @@ interface star {
   r : number // 内半径
   R : number // 外半径
   borderWidth?: number // 描边宽度
+  borderStyle?: string // 描边颜色
+  fill?: boolean //是否填充
   rot?: number //旋转角度
   center?: [number, number] //中心坐标
   fillStyle?: string //填充色
@@ -25,11 +27,14 @@ export default class Star extends vNode {
       this[x] = obj[x]
     }
   }
+  calCenter(): Array<number> {
+    return [this.R * Math.cos(0.1 * Math.PI), this.R]
+  }
 }
 
 Painter
   .reg('STAR', function (node : Star) {
-    let { r, R, rot = 0, borderWidth = 1, fill, fillStyle = '#fff', borderStyle = '#fff', center = [100, 100] } = node
+    let { r, R, rot = 0, borderWidth = 1, fill, fillStyle = '#fff', borderStyle = '#fff', center = node.calCenter() } = node
     this.translate(...center)
     for (var i = 0; i < 5; i++) {
       let x1 = Math.cos((54 + 72 * i + rot) / 180 * Math.PI) * R
